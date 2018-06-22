9c398cb3ceb92f13aa3a5727879898e26885c3d1
Natalie Sandoval <nsandoval@ma.org>
2018-06-22 16:20:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # dog.rb
+
+class Dog
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+end
+  
+
+fido=Dog.new("Fido","Pug")
+
+
+#fido.name="Fido"
+#fido.breed = "Pug"
